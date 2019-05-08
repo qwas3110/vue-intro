741208e01ec967f9b6eea559ed09f69408c8cd06
Laso <laso@LasodeMacBook-Pro.local>
2019-05-08 19:01:58 +0800

    Event Handling

--- js/main.js
+++ js/main.js
@@ -8,16 +8,30 @@ var app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         details: ['80% cotton', '20% Polyester', 'Gender-neutral'],
+        cart: 0,
         variants: [
             {
                 variantId:2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: 'img/green.jpg'
             },
             {
                 variantId:2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: 'img/blue.jpg'
             }
         ]
+    },
+    methods: {
+        addToCart() {
+            this.cart ++;
+        },
+        removeCart() {
+            this.cart !== 0 ? (this.cart --) : (this.cart = 0);
+        },
+        changeColor(img) {
+            this.image = img;
+        }
     }
 });
 
