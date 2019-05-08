1316d17bff9b042422159cc0f8b6f74755bab3ed
Laso <laso@LasodeMacBook-Pro.local>
2019-05-08 16:55:44 +0800

    Attribute Binding

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks'
+        description: 'A pair of warm, fuzzy socks',
+        image: 'img/green.jpg',
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     }
 });
 
