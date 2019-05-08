0166fcf4ea713e541816ab0eda9380522c62140c
Laso <laso@LasodeMacBook-Pro.local>
2019-05-08 18:47:29 +0800

    List Rendering

--- js/main.js
+++ js/main.js
@@ -6,7 +6,18 @@ var app = new Vue({
         image: 'img/green.jpg',
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true
+        inStock: true,
+        details: ['80% cotton', '20% Polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId:2234,
+                variantColor: 'green'
+            },
+            {
+                variantId:2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 });
 
